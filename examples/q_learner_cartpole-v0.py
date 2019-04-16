4beb2308731eabde76be479aaadaac32b3348d8d
BFT\richterbft <christian.richter1@tu-dresden.de>
2019-04-16 11:54:17 +0200

    update

--- examples/q_learner_cartpole-v0.py
+++ examples/q_learner_cartpole-v0.py
@@ -20,64 +20,7 @@ import random
 from fmirl.envs import InvertedPendulum
 from fmirl.agents import QLearner
 
-'''
-class QLearner(object):
-    def __init__(self,
-                 num_states=100,
-                 num_actions=4,
-                 alpha=0.2,
-                 gamma=0.9,
-                 random_action_rate=0.5,
-                 random_action_decay_rate=0.99):
-        self.num_states = num_states
-        self.num_actions = num_actions
-        self.alpha = alpha
-        self.gamma = gamma
-        self.random_action_rate = random_action_rate
-        self.random_action_decay_rate = random_action_decay_rate
-        self.state = 0
-        self.action = 0
-        self.qtable = np.random.uniform(low=-1, high=1, size=(num_states, num_actions))
 
-    def set_initial_state(self, state):
-        """
-        @summary: Sets the initial state and returns an action
-        @param state: The initial state
-        @returns: The selected action
-        """
-        self.state = state
-        self.action = self.qtable[state].argsort()[-1]
-        return self.action
-
-    def move(self, state_prime, reward):
-        """
-        @summary: Moves to the given state with given reward and returns action
-        @param state_prime: The new state
-        @param reward: The reward
-        @returns: The selected action
-        """
-        alpha = self.alpha
-        gamma = self.gamma
-        state = self.state
-        action = self.action
-        qtable = self.qtable
-
-        choose_random_action = (1 - self.random_action_rate) <= np.random.uniform(0, 1)
-
-        if choose_random_action:
-            action_prime = random.randint(0, self.num_actions - 1)
-        else:
-            action_prime = self.qtable[state_prime].argsort()[-1]
-
-        self.random_action_rate *= self.random_action_decay_rate
-
-        qtable[state, action] = (1 - alpha) * qtable[state, action] + alpha * (reward + gamma * qtable[state_prime, action_prime])
-
-        self.state = state_prime
-        self.action = action_prime
-
-        return self.action
-'''
 
 def cart_pole_with_qlearning():
     #env = gym.make('CartPole-v0')
@@ -142,17 +85,11 @@ def cart_pole_with_qlearning():
 
             if done:
                 print("Steps: ", steps)
-                #last_time_steps = np.append(last_time_steps, [int(step + 1)])
-                #if len(last_time_steps) > number_of_iterations_to_average:
-                #    last_time_steps = np.delete(last_time_steps, 0)
                 break
 
         if last_time_steps.mean() > goal_average_steps:
             print("Goal reached!")
             print("Episodes before solve: ", episode + 1)
-            #print(u"Best 100-episode performance {} {} {}".format(last_time_steps.max(),
-            #                                                      unichr(177),  # plus minus sign
-            #                                                      last_time_steps.std()))
             break
 
 
